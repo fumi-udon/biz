400a839b3be6fb7be463d266c6c227e9429e6cea
fumi <fmkz.ysn117@gmail.com>
2023-02-14 19:35:59 +0000

    design only

--- resources/views/dev/rado_simple.blade.php
+++ resources/views/dev/rado_simple.blade.php
@@ -2,38 +2,10 @@
 @extends('layouts.app')
 @section('content')
 <div class="row gy-3 px-3">
-    <div class="p-3 bg-warning text-dark col-md-4">
-    GIT の情報
-    </div>
-    <div class="p-3 bg-warning text-dark col-md-6">
-    https://github.com/fumi-udon/biz.git
-    </div>
-</div>
-
-<div class="row gy-3 px-3">
-    <div class="p-2 bg-warning text-dark col-md-4">
-    Database情報
-    </div>
-    <div class="p-2 bg-warning text-dark col-md-6">
-    .env.production ファイルに記載してます。
-    </div>
-</div>
-
-<div class="row gy-3 px-3">
-    <div class="p-2 bg-warning text-dark col-md-4">
-    関連ファイル
-    </div>
-    <div class="p-2 bg-warning text-dark col-md-6">
-    RadoSimpleController.php / rado_simple.blade.php
-    </div>
-</div>
-
-<div class="row gy-3 px-3">
-    <div class="p-2 bg-warning text-dark col-md-4">
-    ovhのログイン用 ssh
-    </div>
-    <div class="p-2 bg-warning text-dark col-md-6">
-    ssh bistrgr@ssh.cluster031.hosting.ovh.net -p 22
+    <div class="p-2 bg-warning text-dark col-md-12">
+        GIT の情報 : https://github.com/fumi-udon/biz.git<br>
+        Database情報 : .env.production ファイルに記載してます。<br>
+        関連ファイル: RadoSimpleController.php / rado_simple.blade.php<br>
     </div>
 </div>
 @endsection
