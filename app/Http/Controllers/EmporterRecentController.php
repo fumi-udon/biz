cc8de879f95fae2c68ddb528f24bd68b15645ef1
fumi <116494092+fumi-udon@users.noreply.github.com>
2023-03-07 20:40:13 +0100

    キタノ用の実装

--- app/Http/Controllers/EmporterRecentController.php
+++ app/Http/Controllers/EmporterRecentController.php
@@ -22,71 +22,69 @@ class EmporterRecentController extends Controller
      */
     public function index($action_message = null)
     {
-        return view('emporter_recent', compact('action_message'));
+        // select ボックス要素作成
+        $shops = collect([
+            ['id' => 'main', 'name' => 'bistro nippon'],
+            ['id' => 'currykitano', 'name' => 'curry kitano'],
+        ]);
+        $types = collect([
+            ['id' => '1', 'name' => 'all'],
+            ['id' => '2', 'name' => 'emporter'],
+        ]);
+        return view('emporter_recent', compact('action_message','shops','types'));
     }
 
-        /**
-     * 直近アンポルテ表示検索
-     * emporter_recent
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function all_search(Request $request, $btn_id=null, $params=null)
-    {
-        $inputs = $request->all();
-        // リクエストデータ取得
-        $input_date = $inputs['input_date'];
-        // all data
-        $btn_id = 1;
-        //dd($input_date);
-        // session 格納
-        \Session::flash('input_date', $input_date);
-
-        return redirect()->route('search.index',['btn_id' => $btn_id,'input_date' => $input_date])->with([
-            //画面引継ぎsession格納
-            'input_date' => $input_date
-            ]);
-    }
     /**
      * 注文データ全て表示検索
      * emporter_recent
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function search(Request $request, $btn_id=null, $params=null)
+    public function search(Request $request, $type=null, $shop=null)
     {
+        // select ボックス要素作成
+        $shops = collect([
+            ['id' => 'main', 'name' => 'bistro nippon'],
+            ['id' => 'currykitano', 'name' => 'curry kitano'],
+        ]);
+        $types = collect([
+            ['id' => '1', 'name' => 'all'],
+            ['id' => '2', 'name' => 'emporter'],
+        ]);
+
         $inputs = $request->all();
+        // dd($inputs);
         // リクエストデータ取得
         $input_date = $inputs['input_date'];
-        $btn_id = $inputs['btn_id'];
-        Log::debug('xxx: input_date 変数'. $input_date);
-        //dd($input_date);
+        $input_type = $inputs['type_list'];
+        $input_shop = $inputs['shop_list'];
         // session 格納
         \Session::flash('input_date', $input_date);
-        Log::debug('xxx: search'.$btn_id);
-        // [食材消費量] Curl https通信＿SSL エラー回避
-        // $aujourdhui = now()->format("Y-m-d");
+        \Session::flash('type_now', $input_type);
+        \Session::flash('shop_now', $input_shop);
+        // [食材消費量] Curl https通信＿SSL エラー回避 
         $response = Http::withoutVerifying()->get('https://bistronippon.com/api/orders', [
             //'store' => 'currykitano',
-            'store' => 'main',
+            'store' => $input_shop,
             'date' => $input_date,
         ]);
-        
-        $collections = collect($response->json());
+       
+        $collections = collect($response->json());       
         // chat GPT  
         // all / emporter
         $filtered = collect();
-        if(isset($btn_id) && $btn_id == 1){
+        if(isset($input_type) && $input_type == 1){
             //all
-            Log::debug('xxx: all  search_________ '.$btn_id);
+            Log::debug('xxx: all  search_________ '.$input_type);
             $filtered = collect($collections)->where('addition_finished',1)->sortByDesc('end_date')->values()->toArray();
         }else{
-            // emporter 直近3件のみ
+            // emporter:2 直近3件のみ
             Log::debug('xxx: 安保る search kakko');
             $filtered = collect($collections)->where('takeout' ,'==', 1)->where('addition_finished',1)->sortByDesc('end_date')->slice(0, 3)->values()->toArray();
         }
        
         if(empty($filtered)){
             $action_message = 'データ無いよー。ものほんの営業日っすか？';
-            return view('emporter_recent', compact('action_message'));
+            return view('emporter_recent', compact('action_message','shops','types'));
         }
 
         $collection = collect($filtered)->values()->toArray();        
@@ -125,6 +123,12 @@ class EmporterRecentController extends Controller
             }
         });
         // chat GPT edn
-        return view('emporter_recent', compact('result'));
+        return view('emporter_recent', compact('result','shops','types'));
+    }
+
+    // 画面表示用のオブジェクト作成関連
+    public function create_display_objects()
+    {
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,6 @@ Route::post('/bureau_store', [BureauItemsController::class, 'bureau_store'])->na
 
 use App\Http\Controllers\EmporterRecentController;
 Route::get('/emporter_index', [EmporterRecentController::class, 'index'])->name('emporter.index');
-Route::match(['get', 'post'], '/emporter_search', [EmporterRecentController::class, 'search'])->name('search.index');
-Route::post('/all_search/{btn_id?}/{params?}', [EmporterRecentController::class, 'all_search'])->name('allsearch.index');
+Route::post('/emporter_search/{type?}/{shop?}', [EmporterRecentController::class, 'search'])->name('search.index');
+//Route::post('/all_search/{btn_id?}/{params?}', [EmporterRecentController::class, 'all_search'])->name('allsearch.index');
 
