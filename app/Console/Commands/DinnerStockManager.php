1fbed92846eacc0200fb7c3c6588e131e6a31c34
fumi <116494092+fumi-udon@users.noreply.github.com>
2023-05-13 20:30:40 +0100

    ログ

--- app/Console/Commands/DinnerStockManager.php
+++ app/Console/Commands/DinnerStockManager.php
@@ -60,15 +60,18 @@ class DinnerStockManager extends Command
 
         // Rizの消費量取得 [Start]
         $startOfDay = (new DateTime('today'))->setTime(17, 00, 0);
-        $endOfDay = (new DateTime('today'))->setTime(23, 0, 0); 
+        $startOfDayString = $startOfDay->format('Y-m-d H:i:s');
+        $endOfDay = (new DateTime('today'))->setTime(23, 0, 0);
+        $endOfDayString = $endOfDay->format('Y-m-d H:i:s');
         $riz_grammes = AdminConcumedTools::get_riz_stock_data($collections,$startOfDay,$endOfDay);
         // Rizの消費量取得 [END]
 
         // 2 
         // 基準以下の場合はメールを送信
-        // Riz の閾値を超えたらアラートメール送信
+        // Riz の閾値を超えたらアラートメール送信 
         $riz_dline = Config::get('fumi_calc.riz_conso_alart'); 
         logger()->info('[FUMI_cron] 設定値 / 消費量 : '.$riz_dline.' g / '.$riz_grammes.' g');
+        logger()->info('[FUMI_cron] 範囲 : '.$startOfDayString.'  - '.$endOfDayString);
         if($riz_dline < $riz_grammes){
             // Mail 送信 OK 済
             $subject ='Rice alert : conso '.$riz_grammes.' g';
