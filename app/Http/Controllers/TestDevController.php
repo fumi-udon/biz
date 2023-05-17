1c967cd0ed196da30d606a9c5fe75b784cdf10bd
fumi <116494092+fumi-udon@users.noreply.github.com>
2023-05-17 14:41:19 +0100

    2回目送信しよう

--- app/Http/Controllers/TestDevController.php
+++ app/Http/Controllers/TestDevController.php
@@ -25,7 +25,7 @@ class TestDevController extends Controller
 {
     /**
      * 米のストック管理用.
-     *get_riz_stock_data
+     * get_riz_stock_data
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function stock_email()
@@ -48,44 +48,59 @@ class TestDevController extends Controller
         $riz_grammes = AdminConcumedTools::get_riz_stock_data($collections,$startOfDay,$endOfDay);
         // Rizの消費量取得 [END]
 
-        // 2 
         // 基準以下の場合はメールを送信
-        // Riz の閾値を超えたらアラートメール送信 
-        $riz_dline = Config::get('fumi_calc.riz_conso_alart');
-        logger()->info('[Win_タスクスケジューラ] 設定値 / 消費量 : '.$riz_dline.' g / '.$riz_grammes.' g');
-        logger()->info('[Win_タスクスケジューラ] 範囲 : '.$startOfDayString.'  - '.$endOfDayString);
-
-        // メールは一度しか送信しない仕様
         // 今日の日付を取得
         $today = Carbon::today();
         // 今日の日付のレコードをカウント
         $count = ConditionType::whereDate('created_at', $today)->count();
+        $riz_dline = 0;
         if ($count === 0) {
-            // レコードが存在しない場合（メール未送信）
-            if($riz_dline < $riz_grammes){
-                // Mail 送信 処理
-                $subject ='Rice alert : consomation '.$riz_grammes.' g';
-                $body = 'Veuillez vérifier le riz'.'  設定値 / 消費量 : '.$riz_dline.' g / '.$riz_grammes.' g';
-                Mail::to('fuminippon@outlook.com')
-                    ->cc(['satoe1227@gmail.com'])
-                    ->send(new SendinBlueDemoEmail($subject, $body));
-                     logger()->info('[Win_タスクスケジューラ] メール送信しました。');
-
-                    // Mail送信 済/未 判定レコードを作成
-                    $data = [
-                        'type' => 1,
-                        'kubun' => 1,
-                        'numero' => 1,
-                        'color' => 'red',
-                        'sub1' => 1,
-                        'sub2' => 2,
-                    ];    
-                    ConditionType::create($data);
-            }
-        }else{
-            logger()->info('[Win_タスクスケジューラ] メール送信済の為、処理終了');
+            // 未送信
+            $riz_dline = Config::get('fumi_calc.riz_conso_alart');
+            // 閾値を超えたらメール送信して db挿入
+            $this->send_mail_db_reg($riz_dline, $riz_grammes,1);
+        }else if($count === 1){
+            // 1回のみ送信済
+            $riz_dline = Config::get('fumi_calc.riz_conso_alart_2');
+            // 閾値を超えたらメール送信して db挿入
+            $this->send_mail_db_reg($riz_dline, $riz_grammes,2);
+        }else if($count > 1){
+            // 2回送信済
+            logger()->info('[Win_タスクスケジューラ] 処理無し 2回送信済');
         }
 
+        logger()->info('[Win_タスクスケジューラ] 設定値 / 消費量 : '.$riz_dline.' g / '.$riz_grammes.' g');
+        logger()->info('[Win_タスクスケジューラ] 範囲 : '.$startOfDayString.'  - '.$endOfDayString);
+
         return view('dinner_stock', compact('startOfDayString','endOfDayString','riz_dline','riz_grammes','count'));
+    }
+
+    /**
+     * 米の量閾値判定
+     * 
+     * 閾値を超える場合はMail 送信処理　DB レコード登録処理
+     */
+    public function send_mail_db_reg($riz_dline, $riz_grammes, $fois)
+    {
+        if($riz_dline < $riz_grammes){
+            // Mail 送信 処理
+            $subject ='Rice alert : consomation '.$riz_grammes.' g _ '.$fois.'回目';
+            $body = 'dead line:'.$riz_dline.' g';
+            Mail::to('fuminippon@outlook.com')
+                ->cc(['satoe1227@gmail.com'])
+                ->send(new SendinBlueDemoEmail($subject, $body));
+                 logger()->info('[Win_タスクスケジューラ] メール送信しました。');
+
+                // Mail送信 済/未 判定レコードを作成
+                $data = [
+                    'type' => 1,
+                    'kubun' => 1,
+                    'numero' => 1,
+                    'color' => 'red',
+                    'sub1' => 1,
+                    'sub2' => 2,
+                ]; 
+                ConditionType::create($data);
+        }
     }
 }
