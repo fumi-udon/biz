7b8085a8d8f43e04bd652f413c47f584fd5cf4f2
fumi <116494092+fumi-udon@users.noreply.github.com>
2023-05-19 21:22:02 +0100

    note

--- config/fumi_note_alice.php
+++ config/fumi_note_alice.php
@@ -7,6 +7,6 @@ return [
     | Alice 8h こと付け事項
     |--------------------------------------------------------------------------
     */
-    '2023-05-19' =>
+    '2023-05-20' =>
         "Dits mme.kadhija ce matin: vous n'avez pas besoin de préparer du curry, veuillez donc nettoyer la cuisine",
 ];
