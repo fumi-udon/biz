d380c34c861e79e33d2d18bc38c851d0c02d61f1
fumi <116494092+fumi-udon@users.noreply.github.com>
2023-05-19 21:15:34 +0100

    dessin

--- resources/views/recettes_top.blade.php
+++ resources/views/recettes_top.blade.php
@@ -8,29 +8,26 @@
     <div class="d-flex align-items-center p-3 my-3 text-white bg-kitano rounded shadow-sm">
         <img class="me-3" src="{{ asset('img/bootstrap-logo-white.svg') }}" alt="" width="48" height="38">
         <div class="lh-1">
-            <h1 class="h6 mb-0 text-white lh-1">レシピ集</h1>
-            <small>recettes</small>
+            <h1 class="h6 mb-0 text-white lh-1">recettes</h1>
+            <small>レシピ集</small>
         </div>
     </div>
     <div class="row gy-3">
-        <div class="col-md-12">
-            <h5>レシピ集</h5>        
-            <table class="table" id="cry_record" style="">
-                <thead>
-                    <tr>
-                    <th scope="col">Item</th>
-                    <th scope="col">Link</th>
-                    <th scope="col">Note</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>米炊き/ Le riz japonais</td>
-                    <td><a href='riz_jp'>go to page</a></td>
-                    <td></td>
-                </tr>
-                </tbody>
-            </table>
+        <div class="col-md-12">   
+
+        <div class="container">
+        <ul class="list-group">
+            <li class="list-group-item">
+            <div class="row align-items-center">
+                <div class="col">米炊き/ Le riz japonais</div>
+                <div class="col-auto">
+                <a href="riz_jp" class="btn btn-primary">go to page</a>
+                </div>
+            </div>
+            </li>
+        </ul>
+        </div>
+
         </div>
         <div class="col-md-12">
 
