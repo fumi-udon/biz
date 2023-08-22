09aaf3e550b024f68bde7813abdf4e488d3eae27
fumi <116494092+fumi-udon@users.noreply.github.com>
2023-08-22 20:31:49 +0100

    Mail 機能実装

--- app/Console/Commands/RapelleCron.php
+++ app/Console/Commands/RapelleCron.php
@@ -3,7 +3,22 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Config; // Configクラスをインポートする
+use Illuminate\Support\Collection;
 
+// Mail 送信用
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendinBlueDemoEmail;
+use App\FumiLib\AdminConcumedTools;
+use \DateTime; // 追加: PHPのグローバルな名前空間にあるDateTimeクラスを使用することを明示
+use DateTimeZone;
+//Fumi 独自クラス
+use App\FumiLib\FumiTools;
+// Model
+use App\Models\PlanProduction;
+use App\Models\StockIngredient;
+use Carbon\Carbon;
 
 class RapelleCron extends Command
 {
@@ -39,6 +54,36 @@ class RapelleCron extends Command
     public function handle()
     {
         logger()->info('This is FUMI s RapelleCron.');
+        // Fumi 独自クラスインスタンス化
+        $fumi_tools =new FumiTools();
+        $daysoftheweek = $fumi_tools->fumi_get_youbi_for_table(date('w'));
+         
+        // ビレル入力データ取得 flg = 2
+        // 2時間以内の最新レコード取得 (月曜日の場合は日曜日を考慮)
+        $hour_minus = 1;
+        if($daysoftheweek = 'mon'){$hour_minus = $hour_minus + 24;}
+        $stock_record = StockIngredient::where('flg1', 2)
+            ->where('registre_datetime', '>=', Carbon::now()->subHours($hour_minus))
+            ->orderBy('registre_datetime', 'desc')
+            ->first();
+
+        // stock_recordが無い場合は以降するーしてviewをゲット
+        $forget = false;
+        if(empty($stock_record)){
+            //ビレルがデータ登録忘れ
+            $forget = true;
+        }
+
+        if($forget){
+            // Mail 送信 OK 済
+            $subject ='BistroNippon: Bilel forget alart';
+            $body = '閉店時の在庫チェック忘れ。ingrédients à la fermeture';
+            Mail::to('fuminippon@outlook.com')
+                ->cc(['satoe1227@gmail.com'])
+                ->send(new SendinBlueDemoEmail($subject, $body));       
+            logger()->info('[FUMI_cron] Bilel forget Alartメール送信しました。');
+        }
+
         return 0;
     }
 }
