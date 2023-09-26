b08b5d3c668417138918e05fcac9ffe1086225f4
fumi <116494092+fumi-udon@users.noreply.github.com>
2023-09-26 14:34:28 +0100

    ジェイセル

--- resources/views/jesser_works.blade.php
+++ resources/views/jesser_works.blade.php
@@ -35,8 +35,8 @@
 				@endif
 				<!-- 火曜： チェック銀行へ プラン -->
 				@if($daysoftheweek == 'tue')
-					<p> &#x1f5c3; Accédez à la page "gestion des stocks".</p>
-					<p> &#x1f5c3; Si vous avez des chèques à encaisser, veuillez les déposer à la banque Attijari avant le vendredi</p>
+					<p> &#x1f5c3; Accédez à la page "gestion des stocks".</p>					
+					<p> &#x1f5c3; Ménage de salle client: l'étagère et le comptoir et des points sals</p>
 					<p> &#x1f5c3; Achats: demandez un list à Sato</p>
 				@endif
 				<!-- 水曜 チェック銀行へ プラン -->
@@ -49,6 +49,7 @@
 				@if($daysoftheweek == 'thu')
 					<p> &#x1f5c3; Accédez à la page "gestion des stocks".</p>
 					<p> &#x1f5c3; Achats: demandez un list à Sato</p>
+					<p> &#x1f5c3; Ménage de salle client: l'étagère et le comptoir et des points sals</p>
 				@endif
 				<!-- 金曜  プラン -->
 				@if($daysoftheweek == 'fri')
@@ -59,6 +60,10 @@
 				<!-- Steg Sonet 月初め -->
 				@if($daysoftheweek == 'mon' && ( $le_date == '01' || $le_date == '02' || $le_date == '03' || $le_date == '04' || $le_date == '05' || $le_date == '06' || $le_date == '07' || $le_date == '08'))
 					<p> &#x1f6ce; Si vous n'avez pas encore réglé vos factures STEG/SONET/etc du mois dernier, veuillez les payer cette semaine. N'oubliez pas apporter des photos de deux compteur</p>
+				@endif
+				<!-- chéques　月末付近と中旬 -->
+				@if($daysoftheweek == 'tue' && ( $le_date == '15' || $le_date == '16' || $le_date == '17' || $le_date == '18' || $le_date == '19' || $le_date == '20' || $le_date == '21'))
+					<p> &#x1f5c3; Si vous avez des chèques à encaisser, veuillez les déposer à la banque Attijari avant le vendredi</p>
 				@endif
 			</div>
 			<!-- サト追加 -->
