db93daa3bacd669991cf239f5c40f43c3d738750
fumi <116494092+fumi-udon@users.noreply.github.com>
2024-01-07 14:41:35 +0100

    年末用削除

--- resources/views/staff_menu_top.blade.php
+++ resources/views/staff_menu_top.blade.php
@@ -24,7 +24,7 @@
 	</div><!--row end  -->
 
 	<div class="row gx-3 p-3"><!--row start-->
-		<div class="col-md-4 center-block">
+		<!-- <div class="col-md-4 center-block">
 			<ul class="list-group">
 				<li class="list-group-item">
 					<a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vSVgQv9pqQcIwSavzFLFgugv1CbiTk4eCvosm6GffEEoF0d3wxkA29X7GvUdeECuoqOT3Sf_R4ROZmH/pubhtml?gid=242477425&single=true"  target="_blank">planning des horaires</a>				
@@ -33,7 +33,7 @@
 					<a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vSVgQv9pqQcIwSavzFLFgugv1CbiTk4eCvosm6GffEEoF0d3wxkA29X7GvUdeECuoqOT3Sf_R4ROZmH/pubhtml?gid=1692662114&single=true"  target="_blank">changement de menu</a>				
 				</li>
 			</ul>
-		</div>
+		</div> -->
 	</div><!--row end  -->
 </main>
 @endsection
